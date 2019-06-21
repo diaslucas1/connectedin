9547a14c0d5f70dc5387b8c158258aabba969a19
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2019-06-21 17:12:36 -0300

    Creating Perfil page

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+  perfil = Perfil()
+
+  if perfil_id == '1':
+    perfil = Perfil('Lucas Dias', 'lucas@lucas.com.br', '7777777', 'X')
+
+  if perfil_id == '2':
+    perfil = Perfil('Pedro Silva', 'pedro@pedro.com.br', '888888', 'Y')
+
+  return render(request, 'perfil.html', { "perfil" : perfil})
